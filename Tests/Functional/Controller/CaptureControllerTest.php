c64d6bdd254bc857b954e1e41527d92a68a83fce
Lukas Mencl <l.mencl@3sixty.eu>
2020-02-27 20:01:38 +0100

    added missing container in controllers

---
+++ Tests/Functional/Controller/CaptureControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Payum\Bundle\PayumBundle\Tests\Functional\Controller;
+
+use Payum\Bundle\PayumBundle\Tests\Functional\WebTestCase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CaptureControllerTest extends WebTestCase
+{
+    /**
+     * @ticket 507
+     */
+    public function testCanBeAccessed()
+    {
+        $this->expectException(NotFoundHttpException::class);
+        $this->expectExceptionMessage('A token with hash `payum_token` could not be found.');
+
+        $this->client->request('GET', '/payment/capture/payum_token');
+    }
+}
---
+++ Tests/Functional/Controller/SyncControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Payum\Bundle\PayumBundle\Tests\Functional\Controller;
+
+use Payum\Bundle\PayumBundle\Tests\Functional\WebTestCase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class SyncControllerTest extends WebTestCase
+{
+    /**
+     * @ticket 507
+     */
+    public function testCanBeAccessed()
+    {
+        $this->expectException(NotFoundHttpException::class);
+        $this->expectExceptionMessage('A token with hash `payum_token` could not be found.');
+
+        $this->client->request('GET', '/payment/sync/payum_token');
+    }
+}
