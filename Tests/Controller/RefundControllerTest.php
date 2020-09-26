62e0bb57d73aeebb83e8f97a67f49347df1e04d1
David Paz <david.paz@lbbw.de>
2020-09-26 16:11:06 +0200

    Changed tests to inject payum instance
    * Inject payum instance when creating the controllers in tests
    * Extract common code into an abstract test class to avoid code
    duplication

--- Tests/Controller/RefundControllerTest.php
+++ Tests/Controller/RefundControllerTest.php
@@ -1,22 +1,15 @@
 <?php
+
 namespace Payum\Bundle\PayumBundle\Tests\Controller;
 
 use Payum\Bundle\PayumBundle\Controller\RefundController;
 use Payum\Core\GatewayInterface;
-use Payum\Core\Model\Token;
-use Payum\Core\Payum;
-use Payum\Core\Registry\RegistryInterface;
 use Payum\Core\Request\Refund;
-use Payum\Core\Security\GenericTokenFactoryInterface;
-use Payum\Core\Security\HttpRequestVerifierInterface;
-use Payum\Core\Storage\StorageInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class RefundControllerTest extends \PHPUnit\Framework\TestCase
+class RefundControllerTest extends AbstractControllerTest
 {
     /**
      * @test
@@ -33,58 +26,13 @@ class RefundControllerTest extends \PHPUnit\Framework\TestCase
      */
     public function shouldExecuteRefundRequest()
     {
-        $request = Request::create('/');
-        $request->query->set('foo', 'fooVal');
+        $this->initMocks();
+        $controller = new RefundController($this->payum);
 
-        $token = new Token;
-        $token->setGatewayName('theGateway');
-        $token->setAfterUrl('http://example.com/theAfterUrl');
-
-        $httpRequestVerifierMock = $this->createMock(HttpRequestVerifierInterface::class);
-        $httpRequestVerifierMock
-            ->expects($this->once())
-            ->method('verify')
-            ->with($this->identicalTo($request))
-            ->will($this->returnValue($token))
-        ;
-        $httpRequestVerifierMock
-            ->expects($this->once())
-            ->method('invalidate')
-            ->with($this->identicalTo($token))
-        ;
-
-        $gatewayMock = $this->createMock(GatewayInterface::class);
-        $gatewayMock
-            ->expects($this->once())
-            ->method('execute')
-            ->with($this->isInstanceOf(Refund::class))
-        ;
-
-        $registryMock = $this->createMock(RegistryInterface::class);
-        $registryMock
-            ->expects($this->once())
-            ->method('getGateway')
-            ->with('theGateway')
-            ->will($this->returnValue($gatewayMock))
-        ;
-
-        $payum = new Payum(
-            $registryMock,
-            $httpRequestVerifierMock,
-            $this->createMock(GenericTokenFactoryInterface::class),
-            $this->createMock(StorageInterface::class)
-        );
-
-        $container = new Container;
-        $container->set('payum', $payum);
-
-        $controller = new RefundController;
-        $controller->setContainer($container);
-
-        $response = $controller->doAction($request);
+        $response = $controller->doAction($this->request);
 
         $this->assertInstanceOf(RedirectResponse::class, $response);
-        $this->assertEquals('http://example.com/theAfterUrl', $response->getTargetUrl());
+        $this->assertEquals(self::AFTER_URL, $response->getTargetUrl());
     }
 
     /**
@@ -92,57 +40,24 @@ class RefundControllerTest extends \PHPUnit\Framework\TestCase
      */
     public function shouldExecuteRefundRequestWithoutAfterUrl()
     {
-        $request = Request::create('/');
-        $request->query->set('foo', 'fooVal');
+        $this->initMocks();
+        $this->token->setAfterUrl(null);
 
-        $token = new Token;
-        $token->setGatewayName('theGateway');
-        $token->setAfterUrl(null);
+        $controller = new RefundController($this->payum);
 
-        $httpRequestVerifierMock = $this->createMock(HttpRequestVerifierInterface::class);
-        $httpRequestVerifierMock
-            ->expects($this->once())
-            ->method('verify')
-            ->with($this->identicalTo($request))
-            ->will($this->returnValue($token))
-        ;
-        $httpRequestVerifierMock
-            ->expects($this->once())
-            ->method('invalidate')
-            ->with($this->identicalTo($token))
-        ;
-
-        $gatewayMock = $this->createMock(GatewayInterface::class);
-        $gatewayMock
-            ->expects($this->once())
-            ->method('execute')
-            ->with($this->isInstanceOf(Refund::class))
-        ;
-
-        $registryMock = $this->createMock(RegistryInterface::class);
-        $registryMock
-            ->expects($this->once())
-            ->method('getGateway')
-            ->with('theGateway')
-            ->will($this->returnValue($gatewayMock))
-        ;
-
-        $payum = new Payum(
-            $registryMock,
-            $httpRequestVerifierMock,
-            $this->createMock(GenericTokenFactoryInterface::class),
-            $this->createMock(StorageInterface::class)
-        );
-
-        $container = new Container;
-        $container->set('payum', $payum);
-
-        $controller = new RefundController;
-        $controller->setContainer($container);
-
-        $response = $controller->doAction($request);
+        $response = $controller->doAction($this->request);
 
         $this->assertInstanceOf(Response::class, $response);
         $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    protected function initGatewayMock()
+    {
+        $this->gatewayMock = $this->createMock(GatewayInterface::class);
+        $this->gatewayMock
+            ->expects($this->once())
+            ->method('execute')
+            ->with($this->isInstanceOf(Refund::class));
+
     }
 }
